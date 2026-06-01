44bf906e682a3dea88cc82f2a6b297e510af3780
syed-omar-93 <omarshariq1993@gmail.com>
2026-06-01 11:29:39 +0100

    Add the life expectancy staging model

---
+++ models/staging/stg_life_expectancy.sql
@@ -0,0 +1,15 @@
+{{ config(
+    materialized = 'view'
+) }}
+
+select
+    country_name as country,
+    country_code as country_code,
+    year_number as year,
+    value as life_expectancy
+from
+    {{ source('raw', 'life_expectancy_raw') }}
+where    
+    series_name = 'Life expectancy at birth, total (years)'
+    and
+    country_code is not null
