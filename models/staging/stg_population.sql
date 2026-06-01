a9aca0c05f7200e08001a0ba629f8e0890a60f5c
syed-omar-93 <omarshariq1993@gmail.com>
2026-06-01 11:48:25 +0100

    Add the population staging model

---
+++ models/staging/stg_population.sql
@@ -0,0 +1,15 @@
+{{ config(
+    materialized = 'view'
+) }}
+
+select
+    country_name as country,
+    country_code as country_code,
+    year_number as year,
+    value as country_population
+from
+    {{ source('raw', 'population_raw') }}
+where    
+    series_name = 'Population, total'
+    and
+    country_code is not null
